7d9764e82d1685e2c88eafec114a2ea312a0230a
Davi-Hilario <davi.hilario@sptech.school>
2023-10-31 23:14:54 -0300

    atualiazando path do crawler

--- crawler/crawler_BC.py
+++ crawler/crawler_BC.py
@@ -1,6 +1,7 @@
 import requests
 import pandas as pd
 from datetime import date
+import os
 
 
 def start_crawler():
@@ -8,13 +9,15 @@ def start_crawler():
     data_atual = str(date.today()).replace('-', '')
     ano_mes = data_atual[:6]
 
+    abs_path = os.path.abspath(__file__)[:os.path.abspath(__file__).find("crawler")]
+
     link = (f"https://olinda.bcb.gov.br/olinda/servico/Pix_DadosAbertos/versao/v1/odata/TransacoesPixPorMunicipio("
             f"DataBase=@DataBase)?@DataBase='${ano_mes}'&$top=10000&$format=json&$select=AnoMes,Municipio,Estado,"
             f"Regiao,QT_PagadorPF,QT_PagadorPJ,QT_RecebedorPF,QT_RecebedorPJ")
 
-    path_raw = fr"..\analytics\raw\raw{data_atual}.csv"
-    path_trusted = fr"..\analytics\trusted\trusted{data_atual}.csv"
-    path_client = fr"..\analytics\client\client{data_atual}.R"
+    path_raw = abs_path + fr"analytics\raw\raw{data_atual}.csv"
+    path_trusted = abs_path + fr"analytics\trusted\trusted{data_atual}.csv"
+    path_client = abs_path + fr"analytics\client\client{data_atual}.R"
 
     formatted_path_trusted = path_trusted.replace("\\", "/")
 
