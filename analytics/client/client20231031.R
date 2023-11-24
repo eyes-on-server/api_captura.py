079fd7d962cc0523256e3eef171c88077e3ca74e
Davi-Hilario <davi.hilario@sptech.school>
2023-11-23 21:10:17 -0300

    deletando crawler em desuso

--- analytics/client/client20231031.R
+++
@@ -1,3 +0,0 @@
-dataset20231031 <- read.csv2('D:/Davi/Repositorios_Github/Projeto_PI_Segundo_Semestre/api_captura.py/analytics/trusted/trusted20231031.csv', sep=',', header=T)
-dataset20231031$X <- NULL
-View(dataset20231031)
