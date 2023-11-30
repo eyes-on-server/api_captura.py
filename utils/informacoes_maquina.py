f1750a366030a292469143c0f19b1a0f65340b07
Davi-Hilario <davihilario23@gmail.com>
2023-11-29 22:36:45 -0300

    padronizando mac address para ficar igual ao capturado no java, evitando cadastro de servidores iguais

--- utils/informacoes_maquina.py
+++ utils/informacoes_maquina.py
@@ -4,7 +4,8 @@ import psutil as ps
 def get_mac_address():
     for key in ps.net_if_addrs().keys():
         if key.lower().startswith('e'):
-            return ps.net_if_addrs()[key][0].address
+            mac_address = ps.net_if_addrs()[key][0].address
+            return mac_address.replace('-', ':').lower()
 
 
 def get_ipv6():
